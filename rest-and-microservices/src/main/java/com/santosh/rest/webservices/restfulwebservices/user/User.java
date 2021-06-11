1f48ffe4fc783bded10227dba473c1c352c68476
kokasantosh <59968691+kokasantosh@users.noreply.github.com>
2021-06-11 18:00:11 +0530

    Updated the rest service

--- rest-and-microservices/src/main/java/com/santosh/rest/webservices/restfulwebservices/user/User.java
+++ rest-and-microservices/src/main/java/com/santosh/rest/webservices/restfulwebservices/user/User.java
@@ -1,11 +1,15 @@
 package com.santosh.rest.webservices.restfulwebservices.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
     private Integer id;
+    @Size(min=2, message="Name should have atleast 2 characters")
     private String name;
+    @Past
     private Date DOB;
 
     public User() {
--- rest-and-microservices/src/main/java/com/santosh/rest/webservices/restfulwebservices/user/UserResource.java
+++ rest-and-microservices/src/main/java/com/santosh/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -20,11 +21,15 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user == null) {
+            throw new UserNotFoundException("id - " + id);
+        }
+        return user;
     }
 
     @PostMapping("/users")
-    public ResponseEntity createUser(@RequestBody User user) {
+    public ResponseEntity createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -32,4 +37,13 @@ public class UserResource {
                 .buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+        if(user == null) {
+            throw new UserNotFoundException("id - " + id);
+        }
+    }
+
 }
