189221843e862665ae8ff07951bef3d814115475
kokasantosh <59968691+kokasantosh@users.noreply.github.com>
2021-06-12 11:39:53 +0530

    Added config server

---
+++ micro-service/limits-service/src/main/java/com/santosh/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package com.santosh.microservices.limitsservice.controller;
+
+import com.santosh.microservices.limitsservice.bean.Limits;
+import com.santosh.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+//        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
