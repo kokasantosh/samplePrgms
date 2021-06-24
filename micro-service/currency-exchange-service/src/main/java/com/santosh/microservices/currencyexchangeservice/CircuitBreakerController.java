44b55d7618d3d8f97b8429e0ec63480bda93d87d
kokasantosh <59968691+kokasantosh@users.noreply.github.com>
2021-06-24 08:27:26 +0530

    added circuitbreaker controller

---
+++ micro-service/currency-exchange-service/src/main/java/com/santosh/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,12 @@
+package com.santosh.microservices.currencyexchangeservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+    @GetMapping("/sample-api")
+    public String sampleApi() {
+        return "Sample API";
+    }
+}
